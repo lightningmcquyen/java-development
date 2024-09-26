7de1f012224b58b0bf482dcfb8c0f800e6aa2e84
Quyen Vong <qvong97@gmail.com>
2024-09-26 10:49:22 -0400

    completed ShoppingList app

--- workbook-1/ShoppingList/src/main/java/com/pluralsight/shoppinglist/ShoppingList.java
+++ workbook-1/ShoppingList/src/main/java/com/pluralsight/shoppinglist/ShoppingList.java
@@ -2,6 +2,6 @@ package com.pluralsight.shoppinglist;
 
 public class ShoppingList {
     public static void main(String[] args) {
-        System.out.println("Buy stuff");
+        System.out.println("1. milk 2.cereal 3.eggs 4.chicken 5.toothpaste 6.juice 7.water 8.bread 9.toilet paper 10.paper towels");
     }
 }
