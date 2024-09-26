0bba32f407d667b65fe1c9487044802058b12df9
Quyen Vong <qvong97@gmail.com>
2024-09-26 09:54:49 -0400

    completed ShoppingList app

---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        //print hello world to the console/terminal
+        System.out.println("Hello World");
+    }
+
+}
---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("1. milk 2.cereal 3.eggs 4.chicken 5.toothpaste 6.juice 7.water 8.bread 9.toilet paper 10.paper towels");
+    }
+}
+
